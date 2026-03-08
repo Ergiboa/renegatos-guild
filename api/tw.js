a0bb3dfda0fb3754ba536e49a53c63376e4c48bb
Ergiboa <122033958+Ergiboa@users.noreply.github.com>
2026-03-08 02:08:20 +0100

    Update tw.js

--- api/tw.js
+++ api/tw.js
@@ -22,7 +22,7 @@ async function fetchMando(endpoint, body = {}) {
   return res.json();
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
 
