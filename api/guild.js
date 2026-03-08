04f52fbd7b9fc93d7e0784f30c00d31af937a952
Ergiboa <122033958+Ergiboa@users.noreply.github.com>
2026-03-08 02:08:31 +0100

    Update guild.js

--- api/guild.js
+++ api/guild.js
@@ -35,7 +35,7 @@ async function fetchMando(endpoint, body = {}) {
   return res.json();
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   // CORS para desarrollo local
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
